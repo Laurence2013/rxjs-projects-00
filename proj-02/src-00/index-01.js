50ce811fc38f33f696ec375a3b0c1074f4c55a3f
Laurence2013 <lozza244@protonmail.com>
2025-12-22 22:17:14 +0000

    16-commit

--- proj-02/src-00/index-01.js
+++ proj-02/src-00/index-01.js
@@ -4,5 +4,5 @@ const { debounceTime, concatMap, delay } = require('rxjs/operators');
 const searchInput$ = from(['R','X','J','S']).pipe(
 	concatMap(char => of(char).pipe(delay(300)))
 );
-const result$ = searchInput$.pipe(debounceTime(500));
+const result$ = searchInput$.pipe(debounceTime(300));
 result$.subscribe(console.log);
