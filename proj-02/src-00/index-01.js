d6c2090b3deb17688e16c29a8d7fc768b14187a6
Laurence2013 <lozza244@protonmail.com>
2025-12-24 15:39:44 +0000

    17-commit

--- proj-02/src-00/index-01.js
+++ proj-02/src-00/index-01.js
@@ -1,8 +1,21 @@
-const { from, of } = require('rxjs');
-const { debounceTime, concatMap, delay } = require('rxjs/operators');
+const { from, of, zip, interval } = require('rxjs');
+const { debounceTime, concatMap, delay, take, withLatestFrom } = require('rxjs/operators');
 
-const searchInput$ = from(['R','X','J','S']).pipe(
+// 01 - concatMap()
+const obs00$ = from(['R','X','J','S']).pipe(
 	concatMap(char => of(char).pipe(delay(300)))
 );
-const result$ = searchInput$.pipe(debounceTime(300));
-result$.subscribe(console.log);
+//const result$ = obs00$.pipe(debounceTime(300));
+//result$.subscribe(console.log);
+
+// 02 - zip()
+const obs01$ = of('A', 'B');
+const obs02$ = of(1,2,3,4,5);
+const result99$ = zip(obs01$, obs02$);
+//result99$.subscribe(console.log);
+
+// 03 - withLatestFrom()
+const objs03$ = interval(1000).pipe(take(3));
+const objs04$ = of('X', 'Y');
+const result98$ = objs03$.pipe(withLatestFrom(objs04$)); 
+result98$.subscribe(console.log);
